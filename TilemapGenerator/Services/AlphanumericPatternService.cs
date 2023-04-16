d76ddf19fc4f9d4314ffc068d8c6f13ff09f89ff
Simon <simon.vonhoff@outlook.com>
2023-04-16 16:39:45 +0200

    Update AlphanumericPatternService.cs

--- TilemapGenerator/Services/AlphanumericPatternService.cs
+++ TilemapGenerator/Services/AlphanumericPatternService.cs
@@ -79,6 +79,44 @@ namespace TilemapGenerator.Services
         }
 
         /// <summary>
+        /// Extracts all possible patterns of alphanumeric characters from a substring and updates a dictionary with the count of each pattern found.
+        /// </summary>
+        /// <param name="str">The input string to extract patterns from.</param>
+        /// <param name="startIndex">The starting index of the substring within the input string.</param>
+        /// <param name="length">The length of the substring.</param>
+        /// <param name="patternCounts">The dictionary to update with the count of each pattern found.</param>
+        /// <remarks>
+        /// This method extracts all possible patterns of alphanumeric characters from the substring starting at
+        /// <paramref name="startIndex"/> and with a length of <paramref name="length"/>, and updates the dictionary
+        /// <paramref name="patternCounts"/> with the count of each pattern found. Patterns with a length of 1 or less
+        /// are excluded from consideration because they are not considered to be patterns of alphanumeric characters.
+        /// </remarks>
+        private static void ExtractPatterns(ReadOnlySpan<char> str, int startIndex, int length, IDictionary<string, int> patternCounts)
+        {
+            if (length <= 1)
+            {
+                return;
+            }
+
+            for (var i = 2; i <= length; i++)
+            {
+                for (var j = 0; j <= length - i; j++)
+                {
+                    var pattern = str.Slice(startIndex + j, i).ToString();
+
+                    if (patternCounts.ContainsKey(pattern))
+                    {
+                        patternCounts[pattern]++;
+                    }
+                    else
+                    {
+                        patternCounts[pattern] = 1;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
         /// Gets the most occurring letter in a list of strings.
         /// </summary>
         /// <param name="strings">The list of strings to search for letters in.</param>
@@ -91,71 +129,20 @@ namespace TilemapGenerator.Services
         /// </remarks>
         private static string GetMostOccurringLetter(IEnumerable<string> strings)
         {
-            var letterCounts = new Dictionary<char, int>();
+            var letterCounts = strings
+                .Where(str => !string.IsNullOrEmpty(str))
+                .SelectMany(str => str.Where(char.IsLetter))
+                .GroupBy(c => c)
+                .ToDictionary(g => g.Key, g => g.Count());
 
-            foreach (var c in from str in strings from c in str where char.IsLetter(c) select c)
+            if (letterCounts.Count == 0)
             {
-                if (letterCounts.ContainsKey(c))
-                {
-                    letterCounts[c]++;
-                }
-                else
-                {
-                    letterCounts[c] = 1;
-                }
+                return string.Empty;
             }
 
-            var mostCommonLetter = '\0';
-            var mostCommonCount = 0;
-
-            foreach (var (letter, count) in letterCounts)
-            {
-                if (count > mostCommonCount)
-                {
-                    mostCommonLetter = letter;
-                    mostCommonCount = count;
-                }
-            }
+            var mostCommonLetter = letterCounts.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
 
             return mostCommonLetter.ToString();
-        }
-
-        /// <summary>
-        /// Extracts all possible patterns of alphanumeric characters from a substring and updates a dictionary with the count of each pattern found.
-        /// </summary>
-        /// <param name="str">The input string to extract patterns from.</param>
-        /// <param name="startIndex">The starting index of the substring within the input string.</param>
-        /// <param name="length">The length of the substring.</param>
-        /// <param name="patternCounts">The dictionary to update with the count of each pattern found.</param>
-        /// <remarks>
-        /// This method extracts all possible patterns of alphanumeric characters from the substring starting at
-        /// <paramref name="startIndex"/> and with a length of <paramref name="length"/>, and updates the dictionary 
-        /// <paramref name="patternCounts"/> with the count of each pattern found. Patterns with a length of 1 or less 
-        /// are excluded from consideration because they are not considered to be patterns of alphanumeric characters.
-        /// </remarks>
-        private static void ExtractPatterns(string str, int startIndex, int length, IDictionary<string, int> patternCounts)
-        {
-            if (length <= 1)
-            {
-                return;
-            }
-
-            for (var i = 2; i <= length; i++)
-            {
-                for (var j = 0; j <= length - i; j++)
-                {
-                    var pattern = str.Substring(startIndex + j, i);
-
-                    if (patternCounts.ContainsKey(pattern))
-                    {
-                        patternCounts[pattern]++;
-                    }
-                    else
-                    {
-                        patternCounts[pattern] = 1;
-                    }
-                }
-            }
         }
     }
 }
